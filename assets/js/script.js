479c726122667f8e88bb421abe574d3681c9a3dd
Granade <mark.granade@gmail.com>
2021-09-25 12:08:31 -0400

    completed criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,50 @@
-var tasks = {}
-
+// display current 'Day of week', 
 var currentDay = moment().format('dddd, MMMM Do');
 $('#currentDay').text(currentDay);
 
-var createTask = function() {
+// define row's time in 24hour format
+var currentHour = parseInt(moment().format('H'));
+console.log(currentHour);
+var textValue = '';
+var taskId = '';
 
-}
-
-// $(document).ready(function() {
-//     $('textarea').each(function() {
-//         var taskEl = $(this).attr('id');
-//         console.log(taskEl);
-//     });
-// });
-// grabs all <textarea> ID's
-$('textarea').each(function() {
-    var taskEl = $(this).attr('id');
-    console.log(taskEl);
+// get <textarea> ID's
+$(document).ready(function() {
+    $('textarea').each(function() {
+        var taskEl = parseInt($(this).attr('id'));
+        console.log(typeof taskEl);
+        if (taskEl < currentHour) {
+            $(this).addClass('past');
+        }
+        else if (taskEl === currentHour) {
+            $(this).addClass('present');
+        }
+        else {
+            $(this).addClass('future');
+        }
+        // looping through Id's to get local storage
+        // get values and insert them into textarea
+        var storageLocal = JSON.parse(localStorage.getItem(String(taskEl))) || '';
+        $(this).append(storageLocal);
+    });
 });
 
-// console.log(moment().hour())
-// console.log($('textarea'));
+// compare 'currentHour' to each block ID
+// for loop to loop through each block ID
+// if time is in past give style of 'past'
+// else if time is in future give style of 'future'
+// else time is present give style of 'present'
 
-var auditTask = function(taskEl) {
-    var date = $(taskEl)
-        .find(taskEl)
-        .text()
-        .trim();
-    console.log(date);
+// get value of text area when changed
+$('textarea').on('change', function() {
+    textValue = $(this).val(); 
+    console.log(textValue);
+    taskId = $(this).attr('id');
+    console.log(taskId);
+})
 
-    // convert to moment object at 5:00pm
-    var time = moment(date, 'LT');
-    console.log(time);
-
-    // remove any old classes from element
-    $(taskEl).removeClass('present past future');
-
-    // apply new class if task is near/over due date
-    if (moment().isAfter(time)) {
-        $(taskEl).addClass('past');
-    }
-    else if (Math.abs(moment().diff(time, 'days')) <=2) {
-        $(taskEl).addClass('present');
-    }
-};
-
-// var saveTasks = function() {
-//     localStorage.setItem('tasks', JSON.stringify(tasks));
-// };
-
-auditTask();
-// saveTasks();
+// add event listener for save button
+$('.saveBtn').on('click', function() {
+    // send to local storage
+    localStorage.setItem(taskId, JSON.stringify(textValue));
+});
